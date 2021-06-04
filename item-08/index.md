4a0f9730b52969ae5aac530e58144904493fb9d4
Larry Botha <larry@fixate.it>
2021-06-04 15:54:49 +0200

    add examples using zip

--- item-08/index.md
+++ item-08/index.md
@@ -4,3 +4,34 @@
 $ python3 main.py
 ```
 
+- multiple iterables can be processed using `zip`:
+
+  ```python
+  fruits = ['apple', 'pear', 'naartjie']
+  lengths = [len(fruit) for fruit in fruits]
+
+  for fruit, length in zip(fruits, lengths):
+    print(f'{fruit} has {length} characters')
+  ```
+- similar to `enumerate`, `zip` creates a generator from the inputs
+- as in Haskell, if the length of the iterators do not match, any items beyond
+  the length of the shortest list are ommitted from the generator
+- as in Haskell, `zip` consumes list items one at a time, so an infinitely large
+  list can be provided without consuming system memory
+- `zip_longest` from `itertools` can be used as an alternative to the `zip`
+  consuming up until the shortest list:
+
+  ```python
+  xs = [1,2,3]
+  string = "hello"
+  zs = zip_longest(xs, string)
+
+  for _, z in enumerate(zs):
+    print(z)
+
+  # (1,    'h')
+  # (2,    'e')
+  # (3,    'l')
+  # (None, 'l')
+  # (None, 'o')
+  ```
--- item-08/main.py
+++ item-08/main.py
@@ -1,4 +1,7 @@
+from itertools import zip_longest
+from random import randint
 from typing import Optional
+
 
 def cr():
     print("")
@@ -11,5 +14,61 @@ def div(text: Optional[str] = ""):
         div()
 
 
+def _zip_result():
+    div("_zip_result")
+    xs = [randint(0, 10) for _ in list(range(3))]
+    ys = [n ** 2 for n in xs]
+    zs = zip(xs, ys)
+
+    print(f"xs: {xs}")
+    print(f"ys: {ys}")
+
+    print(f"zs:")
+    for _, z in enumerate(zs):
+        print(f"{z!r:>10}")
+    cr()
+
+
+def _parallel_iteration():
+    div("_parallel_iteration")
+    xs = [randint(0, 10) for _ in range(3)]
+    ys = [n ** 2 for n in xs]
+
+    for x, y in zip(xs, ys):
+        print(f"{x}^2 = {y}")
+
+
+def _discarded_values():
+    div("_discarded_values")
+    xs = [randint(0, 10) for _ in list(range(3))]
+    string = "hello"
+    zs = zip(xs, string)
+
+    print(f"xs: {xs!r:>13}")
+    print(f"string: {string}")
+
+    print(f"zs:")
+    for _, z in enumerate(zs):
+        print(f"{z!r:>10}")
+    cr()
+
+
+def _zip_longest():
+    div("_zip_longest")
+    xs = [randint(0, 10) for _ in list(range(3))]
+    string = "hello"
+    zs = zip_longest(xs, string)
+
+    print(f"xs: {xs!r:>13}")
+    print(f"string: {string}")
+
+    print("zs: ")
+    for _, z in enumerate(zs):
+        print(f"{z!r:>15}")
+
 
 if __name__ == "__main__":
+    _zip_result()
+    _parallel_iteration()
+    _discarded_values()
+    _zip_longest()
