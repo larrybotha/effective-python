cb4e4b0998cdb27e2a85cafeb3aa98e1115661e7
Larry Botha <larry@fixate.it>
2021-07-05 08:58:12 +0200

    prepare next section

---
+++ item-36/README.md
@@ -0,0 +1,8 @@
+# Item 36 - `itertools` and working with iterators and generators
+
+```shell
+$ python3 ./
+```
+
+- use `help(itertools)` to see if `itertools` has any useful tools for working
+    with complex or tricky iterators
---
+++ item-36/__main__.py
@@ -0,0 +1,19 @@
+from functools import wraps
+from itertools import count, cycle, repeat
+
+def _print_fn(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        fn_name = fn.__name__.replace("_", " ")
+        indent = 2
+        div = "".join(repeat("=", len(fn_name) + indent * 2))
+        title = "\n".join([div, f"{''.join(repeat(' ', indent))}{fn_name}", div])
+        print(title)
+        fn(*args, **kwargs)
+        print("\n")
+
+    return wrapper
+
+
+
+if __name__ == "__main__":
