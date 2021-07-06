4b6724f3fea3f158ca9431c89ca8544496772d57
Larry Botha <larry@fixate.it>
2021-07-06 08:19:38 +0200

    add notes and examples of itertools classes

--- item-36/README.md
+++ item-36/README.md
@@ -6,3 +6,58 @@ $ python3 ./
 
 - use `help(itertools)` to see if `itertools` has any useful tools for working
     with complex or tricky iterators
+    - `cycle` - emit values from an iterable indefinitely - similar to `replay`
+        in `RxJs`
+
+        ```javascript
+        import {from} from 'rxjs'
+        import {repeat} from `rxjs/operators`
+
+        const stream = from([1,2,3])
+        const repeatStream = stream.pipe(repeat())
+
+        // -1--2--3--1--2--3-->
+        ```
+
+    - `repeat` - emits the same value indefinitely, similar to `constant` in
+        `RxJs`
+    - `count` - emits sequential numbers from a starting point
+    - `zip_longest` - `zip`, but will only emit `StopIteration` when the longest
+        iterable completes
+    - `islice` - iterator slice - slice arbitrary iterable values using a
+        similar syntax to `xs[start:end:step]`, but doesn't support `Reverse`
+    - `chain` - append an iterator onto another, similar to `concat` in
+        Javascript arrays, or in `RxJs`:
+
+        ```javascript
+        import {concat, from} from 'rxjs'
+
+        const x1 = from([1,2,3])
+        const x2 = from([4,5,6])
+        const xs = concat(x1, x2))
+
+        # -1--2--3--4--5--6--|
+        ```
+        - see also `chain.from_iterable`, which accepts a single  iterable of
+        iterables to yield an iterator:
+
+            ```python
+            from itertools import chain
+
+            xxs = [[1,2,3], [4,5,6]]
+            iter = chain.from_iterable(xxs)
+
+            for x in iter:
+                print(x)
+
+            # 1, 2, 3, 4, ...
+            ```
+    - `tee` - create `n` multiple independent iterators from a single iterator
+    - `dropwhile` - drops values from an iterator until a condition is met
+    - `takewhile` - takes values from an iterator until a condition is met
+    - `filterfalse` - inverse of the built-in `filter`
+    - `accumulate` - similar to `RxJs`s `scan`, emitting a value on each
+        iteration of the accumulating function
+    - `product` - generate a cartesian product from iterables
+    - `permutations` - generates permutations of iterables
+    - `combinations` - generates combinations of iterables
--- item-36/__main__.py
+++ item-36/__main__.py
@@ -1,5 +1,16 @@
 from functools import wraps
-from itertools import count, cycle, repeat
+from itertools import (
+    accumulate,
+    chain,
+    count,
+    cycle,
+    dropwhile,
+    repeat,
+    takewhile,
+    tee,
+    zip_longest,
+)
+
 
 def _print_fn(fn):
     @wraps(fn)
@@ -15,5 +26,162 @@ def _print_fn(fn):
     return wrapper
 
 
+@_print_fn
+def _count():
+    counter = count()
+
+    for _ in range(3):
+        print(f"count: {next(counter)}")
+
+
+@_print_fn
+def _cycle():
+    list_cycler = cycle([1, 2])
+
+    for _ in range(4):
+        print(f"list cycle: {next(list_cycler)}")
+
+    print()
+    string_cycler = cycle("cat")
+
+    for _ in range(6):
+        print(f"string cycle: {next(string_cycler)}")
+
+    print()
+    tuple_cycler = cycle((1, 2))
+
+    for _ in range(4):
+        print(f"tuple cycle: {next(tuple_cycler)}")
+
+    print()
+    set_cycler = cycle({1, 2})
+
+    for _ in range(4):
+        print(f"set cycle: {next(set_cycler)}")
+
+    print()
+    gen_cycler = cycle((x for x in range(2)))
+
+    for _ in range(4):
+        print(f"gen cycle: {next(gen_cycler)}")
+
+
+@_print_fn
+def _repeat():
+    repeater = repeat("repeated value", 3)
+
+    for x in repeater:
+        print(f"repeat: {x}")
+
+
+@_print_fn
+def _zip_longest():
+    xs = list(range(3))
+    ys = [x + len(xs) for x in range(len(xs) * 2)]
+
+    print(f"xs: {xs}")
+    print(f"ys: {ys}\n")
+
+    zs = zip_longest(xs, ys, fillvalue="my fill value")
+
+    for z in zs:
+        print(z)
+
+
+@_print_fn
+def _chain():
+    xs = [1, 2, 3]
+    ys = [x + len(xs) for x in xs]
+
+    print(f"xs: {xs}")
+    print(f"ys: {ys}\n")
+
+    chained = chain(xs, ys)
+
+    for x in chained:
+        print(x)
+
+
+@_print_fn
+def _chain_from_iterable():
+    xxs = ["hello", "there"]
+
+    print(f"xxs: {xxs}\n")
+
+    chained = chain.from_iterable(xxs)
+
+    for x in chained:
+        print(x)
+
+
+@_print_fn
+def _tee():
+    xs = [1, 2, 3]
+
+    ys, zs = tee(xs, 2)
+
+    print(f"xs: {xs}")
+
+    print("\nys")
+    for y in ys:
+        print(y)
+
+    print("\nzs")
+    for z in zs:
+        print(z)
+
+
+@_print_fn
+def _dropwhile():
+    xs = list(range(10))
+
+    lte5 = lambda x: x < 5
+    dropper = dropwhile(lte5, xs)
+
+    print(f"xs: {xs}\n")
+
+    print("drop while less than 5")
+
+    for x in dropper:
+        print(x)
+
+
+@_print_fn
+def _takewhile():
+    xs = list(range(10))
+
+    lte5 = lambda x: x < 5
+    taker = takewhile(lte5, xs)
+
+    print(f"xs: {xs}\n")
+
+    print("take while less than 5")
+
+    for x in taker:
+        print(x)
+
+
+@_print_fn
+def _accummulate():
+    xs = [1, 2, 3]
+
+    print(f"xs: {xs}\n")
+
+    sum_squares = lambda acc, x: acc + x ** 2
+    accumulator = accumulate(xs, sum_squares)
+
+    for x in accumulator:
+        print(x)
+
 
 if __name__ == "__main__":
+    _count()
+    _cycle()
+    _repeat()
+    _zip_longest()
+    _chain()
+    _chain_from_iterable()
+    _tee()
+    _dropwhile()
+    _takewhile()
+    _accummulate()
