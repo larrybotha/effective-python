663a54bca425ac219535f00564922d2f34b2b9a2
Larry Botha <larry@fixate.it>
2021-07-07 09:00:44 +0200

    start notes and example for item-37

--- item-37/README.md
+++ item-37/README.md
@@ -4,3 +4,26 @@
 $ python3 ./
 ```
 
+- as soon as one is in the situation where one is tempted to nest dicts inside
+    of dicts, use a class
+- tuples are useful for simple values, but their positions are fixed. If data in
+    a tuple gets more complex, we're left with a long positional value. If a
+    tuple exceeds 2 values, consider either a `namedtuple`, or a class
+
+    - `namedtuple` should only be used if you know no-one is going to access the
+        tuple directly. `namedtuple`s share properties and interfaces with other
+        objects, so users can mistake them for other objects, potentially
+        resulting in unexpected behaviour
+
+        ```python
+        from collections import namedtuple
+
+        Point = namedtuple('Point', ['x', 'y'])
+        p = Point(1,2)
+        print(p)
+        print(p.x)
+        print(p.y)
+        # Point(x=1, y=2)
+        # 1
+        # 2
+        ```
--- item-37/__main__.py
+++ item-37/__main__.py
@@ -1,5 +1,8 @@
+import random
+from collections import defaultdict, namedtuple
 from functools import wraps
 from itertools import repeat
+
 
 def _print_fn(fn):
     @wraps(fn)
@@ -15,4 +18,90 @@ def _print_fn(fn):
     return wrapper
 
 
+@_print_fn
+def _complex_nested_dicts():
+    class ComplexNestedGradeBook:
+        def __init__(self):
+            """make _grades private"""
+            self._grades = {}
+
+        def add_student(self, name: str, /):
+            """
+            here we have a nested dict - this should be a warning sign that a
+            class should be used instead
+            """
+            self._grades[name] = defaultdict(list)
+
+            return self
+
+        def add_grade(
+            self, *, name: str, subject_name: str, grade: float, weight: float
+        ):
+            subject = self._grades[name][subject_name]
+            """tuples are positional
+            If we need more data with this tuple, we'd have to append the item
+            to the end of the tuple, and refactor other locations in the code
+            using this value with positional arguments
+
+            Consider using a namedtuple if the value is never going to be
+            accessed by anyone else, otherwise use a class to describe the new
+            object
+            """
+            subject.append((grade, weight))
+
+            return self
+
+        def get_average_grade(self, name: str, /):
+            subject_grades = self._grades[name].values()
+            grades = []
+
+            """loop within a loop - O(n^2), and difficult to read"""
+            for (i, xs) in enumerate(subject_grades):
+                print(f"subject: {list(self._grades[name].keys())[i]}")
+
+                subject_totals = []
+
+                for (grade, weight) in xs:
+                    subject_totals = subject_totals + [(grade * weight)]
+
+                print(f"subject grades: {subject_totals}\n")
+                grades = grades + subject_totals
+
+            return sum(grades) / len(grades)
+
+    ord_a = ord("a")
+    student_name = "foo"
+    grades = [
+        (chr(x % 2 + ord_a), random.randint(0, 100), random.randint(1, 100) / 100)
+        for x in range(4)
+    ]
+    grade_book = ComplexNestedGradeBook()
+    grade_book.add_student(student_name)
+
+    print(f"student: {student_name}")
+    print(f"grades: {grades}\n")
+
+    for (subject, grade, weight) in grades:
+        grade_book.add_grade(
+            name=student_name, subject_name=subject, grade=grade, weight=weight
+        )
+
+    student_average = grade_book.get_average_grade(student_name)
+
+    print(f"student average: {student_average}")
+
+
+@_print_fn
+def _named_tuple():
+    """this namedtuple could be mistaken for a class"""
+    MyTuple = namedtuple("MyTuple", ["value_a", "value_b"])
+    a = MyTuple(1, 2)
+
+    print(a)
+    print(f"a.value: {a.value_a}")
+    print(f"b.value: {a.value_b}")
+
+
 if __name__ == "__main__":
+    _complex_nested_dicts()
+    _named_tuple()
