c366b8ed0670eb34fb0a4360f96ab05ad0d737e2
Larry Botha <larry@fixate.it>
2021-07-29 09:16:57 +0200

    add notes on @property and setter

--- item-44/README.md
+++ item-44/README.md
@@ -3,3 +3,88 @@
 ```shell
 $ python3 ./
 ```
+
+- avoid using getters and setters for attributes in classes - start with public
+    attributes by default
+    - this makes incrementing / decrementing attributes simple
+    - this also allows one to migrate to the `@property` decorator at a later
+        stage without breaking the implementation:
+
+        ```python
+        class MyClass:
+          def __init__(self, value):
+            self.value = value
+
+        instance == MyClass(5)
+        print(instance.value) # 5
+        instance.value = 10
+        print(instance.value) # 10
+
+        # some time, later, when we need to refactor MyClass
+        class MyClass:
+            def __init__(self, value)
+                self._value
+
+            @property
+            def value(self):
+                return self._value
+
+            @value.setter
+            def value(self, value):
+                self._value = value
+
+        # interface is preserved
+        instance == MyClass(5)
+        print(instance.value) # 5
+        instance.value = 10
+        print(instance.value) # 10
+        ```
+- the `@property` decorator allows one to define property getters and setters in
+    2 parts:
+
+    ```python
+    class MyClass:
+        @property
+        def public_property_name(self):
+            return self._some_value
+
+        @public_property_name.setter
+        def public_property_name(self, value):
+            self._some_value = value
+    ```
+- `@property` can be used for side effects:
+
+    ```python
+    class MyClass:
+        @property
+        def public_property_name(self):
+            return self._some_value
+
+        @public_property_name.setter
+        def public_property_name(self, value):
+            print('a side effect!')
+            self._some_value = value
+    ```
+- the `setter` property can be used for validation:
+
+    ```python
+    class IntValidator:
+        def __init__(self, value):
+            # use the setter to validate instantiation
+            self.value = value
+
+        @property
+        def value(self):
+            return self._value
+
+        @value.setter(self,value):
+            if not isinstance(value, int):
+                raise TypeError('only int allowed')
+
+            self._value = value
+
+    int_validator = IntValidator(1) # valid
+    int_validator.value = .5 # invalid
+
+    IntValidator(.5) # also invalid
+    ```
--- item-44/__main__.py
+++ item-44/__main__.py
@@ -16,5 +16,150 @@ def _print_fn(fn):
     return wrapper
 
 
+@_print_fn
+def _prefer_public_attributes_over_getters_and_setters():
+    class MyClassWithSetterAndGetter:
+        def __init__(self):
+            self._some_value = 5
+
+        def get_some_value(self):
+            return self._some_value
+
+        def set_some_value(self, value):
+            self._some_value = value
+
+            return self._some_value
+
+    class MyClassWithPublicAttributes:
+        def __init__(self):
+            self.some_value = 5
+
+    instance_1 = MyClassWithSetterAndGetter()
+    string_1 = "instance_1.get_some_value()"
+    string_2 = "instance_1.set_some_value(10)"
+
+    print(f"{string_1}: {eval(string_1)}")
+    print(f"{string_2}: {eval(string_2)}")
+
+    instance_2 = MyClassWithPublicAttributes()
+    string_1 = "instance_2.some_value"
+    string_2 = "instance_2.some_value = 10"
+
+    print(f"{string_1}: {eval(string_1)}")
+    instance_2.some_value = 10
+    print(f"{string_2}: {instance_2.some_value}")
+
+
+@_print_fn
+def _starting_with_public_attributes_allows_one_to_migrate_to_property_decorator():
+    class MyClass:
+        def __init__(self, value):
+            self.value = value
+
+    print("MyClass with public attribute")
+    instance_1 = MyClass(5)
+    string_1 = "instance_1.value"
+    print(f"{string_1}: {eval(string_1)}")
+
+    instance_1.value = 10
+    string_2 = "instance_1.value"
+
+    print(f"{string_2}: {eval(string_2)}")
+
+    # later in time...
+    class MyClass:
+        def __init__(self, value):
+            self._value = value
+
+        @property
+        def value(self):
+            return self._value
+
+        @value.setter
+        def value(self, value):
+            self._value = value
+
+    print("\nMyClass with @property attribute")
+    instance_1 = MyClass(5)
+    string_1 = "instance_1.value"
+    print(f"{string_1}: {eval(string_1)}")
+
+    instance_1.value = 10
+    string_2 = "instance_1.value"
+
+    print(f"{string_2}: {eval(string_2)}")
+
+
+@_print_fn
+def _property_decorator_allows_for_side_effects():
+    class PropertyAndSideEffect:
+        def __init__(self, value):
+            self._value = value
+            self.something = self._value * 2
+
+        @property
+        def value(self):
+            return self._value
+
+        @value.setter
+        def value(self, value):
+            self._value = value
+            # side effect
+            print("side effect after setting value!")
+            self.something = self._value * 2
+
+    instance = PropertyAndSideEffect(5)
+
+    string_1 = "instance.value"
+    string_2 = "instance.something"
+    print(f"{string_1}: {eval(string_1)}")
+    print(f"{string_2}: {eval(string_2)}\n")
+
+    instance.value = 10
+    print(f"{string_1}: {eval(string_1)}")
+    print(f"{string_2}: {eval(string_2)}")
+
+
+@_print_fn
+def _setter_property_can_be_used_for_validation():
+    class Natural:
+        def __init__(self, value=1, /):
+            # use the setter to set the value
+            self.value = value
+
+        @property
+        def value(self):
+            return self._value
+
+        @value.setter
+        def value(self, value: int):
+            if value < 1:
+                raise ValueError("cannot be less than 1")
+
+            self._value = value
+
+    natural = Natural()
+
+    string_1 = "natural.value"
+    print(f"{string_1}: {eval(string_1)}")
+
+    try:
+        print("\nattempt to set value < 1")
+        natural.value = 0
+    except ValueError as error:
+        print(f"\nError: {error.__class__.__name__}")
+        print(error)
+
+    try:
+        print("\ninstantiate with value < 1")
+        natural = Natural(0)
+    except ValueError as error:
+        print(f"\nError: {error.__class__.__name__}")
+        print(error)
+
+
 if __name__ == "__main__":
-    pass
+    _prefer_public_attributes_over_getters_and_setters()
+    _starting_with_public_attributes_allows_one_to_migrate_to_property_decorator()
+    _property_decorator_allows_for_side_effects()
+    _setter_property_can_be_used_for_validation()
