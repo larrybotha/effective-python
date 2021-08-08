f0c8507fefd04c5019a7dabcac202c49d436ec7f
Larry Botha <larry@fixate.it>
2021-08-08 13:26:11 +0200

    add notes on using @property to patch a class

--- item-45/README.md
+++ item-45/README.md
@@ -4,3 +4,47 @@
 $ python3 ./
 ```
 
+- the Javascript equivalent of `Date.now()` in Python is
+    `datetime.datetime.now()`
+- `datetime.timedelta` is useful for managing time deltas, rather than relying
+    on an integer
+- `getattr` and `setattr` can be used to dynamically get or set values on a
+    class:
+
+    ```python
+    class MyClass:
+        def __init__(self):
+            self.some_value = 0
+
+        def print(self):
+            print(getattr(self, 'some_value'))
+    ```
+- one can use `@property` to patch classes:
+
+    ```python
+    class MyBadClass:
+        def __init__(self, value: int, /):
+            self.value = value
+            # only set when initialised
+            self._value_squared = self.value ** 2
+
+    class MyPatchedClass(MyBadClass):
+        def __init__(self, value: int, /):
+            super().__init__(value)
+            self._value = value
+
+        @property
+        def value(self):
+            return self._value
+
+        @value.setter
+        def value(self, value: int, /):
+            self._value = value
+            # set _value_squared when value is changed
+            # NOTE: this is a side effect - it would be better to use a method
+            # to do this
+            self._value_squared = self.value ** 2
+    ```
+- repeated use of `@property` in a class likely indicates the class should be
+    rewritten
+- `@property` is useful for incrementally adding behaviour to a class
--- item-45/__main__.py
+++ item-45/__main__.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from functools import wraps
 from itertools import repeat
 
@@ -16,5 +17,117 @@ def _print_fn(fn):
     return wrapper
 
 
+@_print_fn
+def _use_property_to_patch_poorly_implemented_classes():
+    class Bucket:
+        def __init__(self, period: int, /):
+            self.period_delta = timedelta(seconds=period)
+            self.reset_time = datetime.now()
+            self.quota = 0
+
+        def __repr__(self):
+            return f"Bucket(quota={self.quota})"
+
+    def fill(bucket: Bucket, amount: int, /):
+        """fill.
+
+        fill a bucket with an amount
+
+        if the time since the reset time is greater than the period this bucket
+        is eligible to have a quota, then reset the quota
+
+        :param bucket:
+        :type bucket: Bucket
+        :param amount:
+        :type amount: int
+        """
+        now = datetime.now()
+
+        if (now - bucket.reset_time) > bucket.period_delta:
+            bucket.quota = 0
+            bucket.reset_time = now
+
+        bucket.quota += amount
+
+    def deduct(bucket: Bucket, amount: int, /):
+        now = datetime.now()
+
+        if (now - bucket.reset_time) > bucket.period_delta:
+            return False
+
+        if amount > bucket.quota:
+            return False
+
+        bucket.quota -= amount
+
+        return True
+
+    bucket = Bucket(1)
+    print(f"after instantiation: {bucket}")
+
+    fill(bucket, 100)
+    print(f"after fill 100: {bucket}")
+
+    deduct(bucket, 99)
+    print(f"after deduct 99: {bucket}")
+
+    deduct(bucket, 3)
+    print(f"after deduct 3: {bucket}")
+    print(
+        "\nat this point we don't know why we can't deduct anymore because the"
+        "Bucket implementation doesn't indicate what quota we started with."
+        "To fix this, we can use @property and subclassing to create an improved"
+        "Bucket class that retains the same interface as Bucket\n"
+    )
+
+    class PatchedBucket(Bucket):
+        def __init__(self, period: int = 1, /):
+            self.max_quota = 0
+            self.quota_consumed = 0
+            super().__init__(period)
+
+        def __repr__(self):
+            attributes = [
+                f"\t{attr}={getattr(self, attr)}"
+                for attr in ["quota", "quota_consumed"]
+            ]
+            xs = ["PatchedBucket(", *attributes, ")"]
+            string = "\n".join(xs)
+
+            return string
+
+        @property
+        def quota(self):
+            return self.max_quota - self.quota_consumed
+
+        @quota.setter
+        def quota(self, amount: int, /):
+            delta = self.max_quota - amount
+
+            if amount == 0:
+                self.quota_consumed = 0
+                self.max_quota = 0
+
+            elif delta < 0:
+                assert self.quota_consumed == 0
+                self.max_quota = amount
+
+            else:
+                assert self.max_quota >= self.quota_consumed
+                self.quota_consumed += delta
+
+    bucket = PatchedBucket(1)
+    print(f"after instantiation:\n{bucket}\n")
+
+    fill(bucket, 100)
+    print(f"after fill 100:\n{bucket}\n")
+
+    deduct(bucket, 99)
+    print(f"after deduct 99:\n{bucket}\n")
+
+    deduct(bucket, 3)
+    print(f"after deduct 3:\n{bucket}\n")
+
+
 if __name__ == "__main__":
-    pass
+    _use_property_to_patch_poorly_implemented_classes()
