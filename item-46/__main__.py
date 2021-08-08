7957549e4a733c646e5fdf0cbef91b6290d4224e
Larry Botha <larry@fixate.it>
2021-08-08 20:54:58 +0200

    begin showing limitations of multiple property declarations

--- item-46/__main__.py
+++ item-46/__main__.py
@@ -16,5 +16,38 @@ def _print_fn(fn):
     return wrapper
 
 
+@_print_fn
+def _property_not_good_for_reuse():
+    class Homework:
+        def __init__(self):
+            self._grade = 0
+
+        @property
+        def grade(self):
+            return self._grade
+
+        @grade.setter
+        def grade(self, value: int, /):
+            """grade.
+
+            validate that the homework's grade is between 0 and 100
+
+            :param value:
+            :type value: int
+            """
+            if not (0 <= value <= 100):
+                raise ValueError("grade must be between 0 and 100")
+
+            self._grade = value
+
+        def __repr__(self):
+            return f"Homework(grade={self.grade})"
+
+    kid_a_homework = Homework()
+    kid_a_homework.grade = 60
+
+    print(kid_a_homework)
+
+
 if __name__ == "__main__":
-    pass
+    _property_not_good_for_reuse()
