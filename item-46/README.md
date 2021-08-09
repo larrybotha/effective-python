cf504461ebc6f212a10a242892f3590eaa57bfc2
Larry Botha <larry@fixate.it>
2021-08-09 09:15:30 +0200

    begin showing shortfalls of @property for multiple attributes

--- item-46/README.md
+++ item-46/README.md
@@ -4,3 +4,17 @@
 $ python3 ./
 ```
 
+- `@property` is good for defining getters and setters for a single property
+    with validation
+- `@property` is not so good for multiple properties that should behave in the
+    same manner - i.e. reusing logic `@property` is not possible - the same
+    logic must be defined repeatedly
+- using a descriptor class to define shared behaviour of properties within a
+    class can be superior
+- to define a descriptor class, create a class that defines a `__set__` and
+    `__get__` method:
+
+    ```python
+    def MyDescriptor:
+      def __set__(self):
+    ```
--- item-46/__main__.py
+++ item-46/__main__.py
@@ -17,7 +17,7 @@ def _print_fn(fn):
 
 
 @_print_fn
-def _property_not_good_for_reuse():
+def _property_ok_for_single_use():
     class Homework:
         def __init__(self):
             self._grade = 0
@@ -49,5 +49,50 @@ def _property_not_good_for_reuse():
     print(kid_a_homework)
 
 
+@_print_fn
+def _property_not_good_for_general_use():
+    class Exam:
+        def __init__(self):
+            self._writing_grade = 0
+            self._math_grade = 0
+
+        def __repr__(self):
+            return f"Exame(writing_grade={self.writing_grade}, math_grade={self.math_grade})"
+
+        @staticmethod
+        def check_grade(grade: int, /):
+            if not (0 <= grade <= 100):
+                raise ValueError("grade must be between 0 and 100")
+
+        # using @property we repeat the getters and setters here - this is
+        # tedious for an arbitrary number of items
+        @property
+        def writing_grade(self):
+            return self._writing_grade
+
+        @writing_grade.setter
+        def writing_grade(self, grade: int, /):
+            self.check_grade(grade)
+            self._writing_grade = grade
+
+        @property
+        def math_grade(self):
+            return self._math_grade
+
+        @math_grade.setter
+        def math_grade(self, grade: int, /):
+            self.check_grade(grade)
+            self._math_grade = grade
+
+    kid_a_exam = Exam()
+    kid_a_exam.writing_grade = 60
+    kid_a_exam.math_grade = 50
+
+    print(kid_a_exam)
+
+    print(f"\nlots of repetition here to define different subjects")
+
+
 if __name__ == "__main__":
-    _property_not_good_for_reuse()
+    _property_ok_for_single_use()
+    _property_not_good_for_general_use()
