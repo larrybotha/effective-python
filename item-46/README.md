2479552157c24cadb88d4d39f1201e703b531881
Larry Botha <larry@fixate.it>
2021-08-22 17:08:30 +0200

    add notes and examples on using WeakKeyDictionary to prevent memory leaks

--- item-46/README.md
+++ item-46/README.md
@@ -11,10 +11,134 @@ $ python3 ./
     logic must be defined repeatedly
 - using a descriptor class to define shared behaviour of properties within a
     class can be superior
+- **NB**: descriptors are assigned as class attributes!
 - to define a descriptor class, create a class that defines a `__set__` and
     `__get__` method:
 
     ```python
     def MyDescriptor:
-      def __set__(self):
+        def __get__(self, instance, instance_type):
+            pass
+
+        def __set__(self, instance):
+            pass
     ```
+- given the following code:
+
+    ```python
+    class MyDescriptor:
+        def __init__(self, value = 0, /)
+            self._value = value
+
+        def __get__(self, instance, instance_type):
+            return self._value
+
+        def __set__(self, instance, value):
+            self._value = value
+
+    class MyClass:
+        some_value = MyDescriptor()
+
+    # 1 - create instance
+    class_instance = MyClass()
+
+    # 2 - assign attribute
+    class_instance.some_value = 5
+
+    # 3 - get attribute
+    print(class_instance.some_value)
+    # => 5
+    ```
+
+    - assigning the attribute (#2) is interpreted as follows:
+
+        ```python
+        MyClass.__dict__['some_value'].__set__(class_instance, 5)
+        ```
+
+    - getting an attribute (#3) is interpreted as follows:
+
+        ```python
+        MyClass.__dict__['some_value'].__get__(class_instance, MyClass)
+        ```
+    - Python does the following when accessing or assigning attributes:
+        1. checks if an instance has the attribute
+        2. checks the instance's class for the attribute
+            - if the attribute implements the descriptor interface, i.e. it
+                defines a `__get__` and `__set__` method, Python assumes you
+                want to follow the descriptor protocol
+
+        i.e.
+
+        ```python
+        class_instance.some_value
+
+        # first evaluate class instance:
+        # class_instance.__dict__['some_value']
+
+        # else evaluate class itself:
+        # MyClass.__dict__['some_value']
+        # or
+        # class_instance.__class__.__dict__['some_value']
+        ```
+    - property access is similar here to Javascript's prototypal inheritance /
+        delegation
+- beware when using descriptors with an `__init__` - the init is called when the
+    class is evaluated, and behaves as a class attribute:
+
+    ```python
+    class MyDescriptor:
+        def __init__(self, value = "foo", /):
+            self._value = value
+
+        def __get__(self, *_):
+            return self._value
+
+        def __set__(self, instance, value, /):
+            self._value = value
+
+    class MyClass:
+        value = MyDescriptor()
+
+    instance_1 = MyClass()
+    instance_2 = MyClass()
+
+    instance_1.value = "bar"
+
+    # instance_2 will have the following:
+    # instance_2.value == "bar"
+    ```
+
+    Descriptors are useful for computing values from existing instance
+    properties, so relying on `__init__`, unless setting a value for a class
+    attribute, is not going to work as expected
+- to ensure that attributes are attribute-specific with descriptors, one
+    should use `WeakKeyDictionary` to store values for all the instances
+    inside the descriptor (an example demonstrating a memory leak, as a
+    result of using a `dict` to store references to instances, can be seen
+    in the examples)
+
+    ```python
+    from weakref import WeakKeyDictionary
+
+    class MyDescriptor:
+        def __init__(self):
+            # this prevents references to instances being retained when instance
+            # counts reach zero, as opposed to using dict
+            self._values = WeakKeyDictionary()
+
+        def __get__(self, instance, _):
+            if instance is None:
+                return self
+
+            return self._values.get(instance, "default value")
+
+        def __set__(self, instance, value):
+            self._values[instance] = value
+    ```
+
+    - `WeakKeyDictionary` holds weak references to objects - when there are no
+        more strong references to objects, i.e. the object is no longer used
+        elsewhere, the value in the `WeakKeyDictionary` will be garbage
+        collected, as opposed to if we used a `dict` or something that would
+        hold a strong reference to the value
--- item-46/__main__.py
+++ item-46/__main__.py
@@ -1,5 +1,7 @@
 from functools import wraps
 from itertools import repeat
+from random import randint
+from weakref import WeakKeyDictionary
 
 
 def _print_fn(fn):
@@ -57,7 +59,7 @@ def _property_not_good_for_general_use():
             self._math_grade = 0
 
         def __repr__(self):
-            return f"Exame(writing_grade={self.writing_grade}, math_grade={self.math_grade})"
+            return f"Exam(writing_grade={self.writing_grade}, math_grade={self.math_grade})"
 
         @staticmethod
         def check_grade(grade: int, /):
@@ -93,6 +95,147 @@ def _property_not_good_for_general_use():
     print(f"\nlots of repetition here to define different subjects")
 
 
+@_print_fn
+def _descriptor_example():
+    class MyDescriptor:
+        def __init__(self, value=0, /):
+            self._value = value
+
+        def __get__(self, instance, instance_type):
+            print("getting value")
+            print(f"instance: {instance}")
+            print(f"instance_type: {instance_type.__name__}\n")
+
+            return self._value
+
+        def __set__(self, instance, value):
+            print(f"setting value: {value}")
+            print(f"instance: {instance}\n")
+            self._value = value
+
+    class MyClass:
+        some_value = MyDescriptor()
+
+    instance = MyClass()
+
+    print(f"instance.some_value: {instance.some_value}\n")
+
+    instance.some_value = 5
+
+
+@_print_fn
+def _careful_of_class_attributes_sharing_values():
+    class MyDescriptor:
+        def __init__(self, value: str = "foo", /):
+            self._value = value
+
+        def __get__(self, *_):
+            return self._value
+
+        def __set__(self, _, value: str):
+            self._value = value
+
+    class MyClass:
+        class_attr = MyDescriptor()
+
+        def __repr__(self):
+            return f"{self.__class__.__name__}(class_attr: {self.class_attr})"
+
+    instance_1 = MyClass()
+    instance_2 = MyClass()
+
+    print(f"instance_1: {instance_1}")
+    print(f"instance_2: {instance_2}\n")
+
+    print("setting instance_1.class_attr = 'bar'\n")
+    instance_1.class_attr = "bar"
+
+    print(f"instance_1: {instance_1.class_attr}")
+    print(f"instance_2: {instance_2.class_attr}\n")
+
+    print("instance_2.class_attr changed!")
+
+
+@_print_fn
+def _memory_leaking_implementation_with_dict():
+    class Grade:
+        def __init__(self):
+            # create a dict that we can dynamically assign grades for each
+            # instance to
+            self._values = {}
+
+        def __get__(self, instance, _):
+            # if there's no instance, return the descriptor
+            if instance is None:
+                return self
+
+            # from the descriptor's _values attribute, get the value at the key
+            # `instance`, otherwise 0
+            return self._values.get(instance, 0)
+
+        def __set__(self, instance, value):
+            if not (0 <= value <= 100):
+                return ValueError("grade must be between 0 and 100")
+
+            # Set the grade on the _values dict using the instance as the key
+            # _values will hold a reference to each instance, even after the
+            # instance is no longer needed. This prevents the garbage collector
+            # from cleaning up, resulting in a memory leak - the instances will
+            # persist for the duration of the application running
+            print(
+                "reference to instance created in descriptor - memory leak introduced"
+            )
+            self._values[instance] = value
+
+    class Student:
+        math_grade = Grade()
+
+    student_1 = Student()
+    student_2 = Student()
+
+    student_1.math_grade = randint(0, 100)
+    student_2.math_grade = randint(0, 100)
+
+    print(f"\nstudent 1 math: {student_1.math_grade}")
+    print(f"student 2 math: {student_2.math_grade}")
+
+
+@_print_fn
+def _memory_safe_implementation_with_weakkeydictionary():
+    class Grade:
+        def __init__(self):
+            print("WeakKeyDictionary instantiated - values will be garbage collected")
+            self._values = WeakKeyDictionary()
+
+        def __get__(self, instance, _):
+            if instance is None:
+                return self
+
+            self._values.get(instance, 0)
+
+        def __set__(self, instance, value):
+            if not (0 <= value <= 100):
+                raise ValueError("grade must be between 0 and 100")
+
+            self._values[instance] = value
+
+    class Student:
+        math_grade = Grade()
+
+    student_1 = Student()
+    student_2 = Student()
+
+    student_1.math_grade = randint(0, 100)
+    student_2.math_grade = randint(0, 100)
+
+    print(f"\nstudent 1 math: {student_1.math_grade}")
+    print(f"student 2 math: {student_2.math_grade}")
+
+
 if __name__ == "__main__":
     _property_ok_for_single_use()
     _property_not_good_for_general_use()
+    _descriptor_example()
+    _careful_of_class_attributes_sharing_values()
+    _memory_leaking_implementation_with_dict()
+    _memory_safe_implementation_with_weakkeydictionary()
